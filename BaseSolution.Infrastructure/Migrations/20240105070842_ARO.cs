dffd3d90037b92557c7f329baed2d997833fb27a
SNAKE01112004 <nguyendoanquangluc1112004@gmail.com>
2024-01-09 22:49:48 +0700

    first commit

---
+++ BaseSolution.Infrastructure/Migrations/20240105070842_ARO.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseSolution.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ARO : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
